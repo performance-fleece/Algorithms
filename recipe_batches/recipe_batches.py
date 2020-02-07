06a451a3d578fa959a7f955357313b6672ab98c2
Jeremy Morales <jeremy.morales@gmail.com>
2020-02-07 02:04:00 -0800

    recipe passes test

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -30,10 +30,3 @@ def recipe_batches(recipe, ingredients):
 # recipe = {'milk': 100, 'butter': 50, 'cheese': 10}
 # ingredients = {'milk': 198, 'butter': 52, 'cheese': 10}
 # recipe_batches(recipe, ingredients)
-if __name__ == '__main__':
-    # Change the entries of these dictionaries to test
-    # your implementation with different inputs
-    recipe = {'milk': 100, 'butter': 50, 'flour': 5}
-    ingredients = {'milk': 132, 'butter': 48, 'flour': 51}
-    print("{batches} batches can be made from the available ingredients: {ingredients}.".format(
-        batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
